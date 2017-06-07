e165da161dda67ac444630d79b030980ff85b692
Camilo Benavides <novadevco@gmail.com>
2017-06-07 18:59:03 -0500

    Creación unidad 3

---
+++ resources/views/unidades/unidad3/unidad3.blade.php
@@ -0,0 +1,248 @@
+@extends('layouts.app')
+@section('css')
+    <link href="{{ asset('/assets/css/tree.css') }}" rel="stylesheet">
+@endsection
+@section('content')
+<div class="container bs-docs-container">
+   <div class="row">
+      <div class="col-md-9" role="main">
+         <div class="bs-docs-section">
+            <h1 class="page-header" id="overview">EL SECTOR SOLIDARIO Y SUS ORGANIZACIONES</h1>
+            <p align="justify">Las organizaciones del sector solidario surgen por iniciativa de una comunidad que decide asociarse a trav&eacute;s de la cooperaci&oacute;n y la ayuda mutua, uniendo recursos humanos y econ&oacute;micos como herramientas para resolver los problemas y/o necesidades o desarrollar proyectos productivos comunes para el bienestar de ese colectivo.</p>
+            <img src="assets/images/map.png" width="100%" alt="" style="padding:40px;">
+            <p align="justify"><strong>ORGANIZACIONES SOLIDARIAS DE DESARROLLO:</strong> Son aquellas formas de asociatividad solidaria sin &aacute;nimo de lucro, que, empleando bienes y servicios privados o gubernamentales, construyen y prestan bienes y servicios para el beneficio social, dirigiendo su accionar principalmente hacia terceros, comunidades y la sociedad en general. La finalidad de su constituci&oacute;n es ejercer la solidaridad principalmente de adentro hacia afuera.</p>
+            <p align="justify"><strong>ORGANIZACIONES DE ECONOM&Iacute;A SOLIDARIA (Caracter&iacute;sticas)</strong></p>
+            <p align="justify">&bull; Organizarse como empresa que contemple en su objeto social el ejercicio de una actividad socioecon&oacute;mica, tendiente a satisfacer necesidades de sus asociados y el desarrollo de obras de servicio comunitario.</p>
+            <p align="justify">&bull; Establecer un v&iacute;nculo asociativo fundado en los principios y fines de la econom&iacute;a solidaria.</p>
+            <p align="justify">&bull; Incluir en sus estatutos o reglas b&aacute;sicas de funcionamiento la ausencia de &aacute;nimo de lucro movida por la solidaridad y el servicio social o comunitario.</p>
+            <p align="justify">&bull; Garantizar la igualdad de derechos y obligaciones de sus miembros sin consideraci&oacute;n a sus aportes.</p>
+            <p align="justify">&bull; Establecer en sus estatutos un monto m&iacute;nimo de aportes sociales no reducibles durante su existencia y debidamente pagados.</p>
+            <p align="justify">&bull; Integrarse social y econ&oacute;micamente, sin perjuicio de sus v&iacute;nculos, con otras entidades sin &aacute;nimo de lucro que tengan por fin promover el desarrollo integral del ser humano.</p>
+            <!--3.1.-->
+            <h2 class="subtUnidad">3.1. COOPERATIVAS</h2>
+            <p align="justify">Características de las Cooperativas</p>
+            <table class="table table-striped" cellspacing="0" cellpadding="7">
+                <tbody>
+                <tr valign="top">
+                <td style="background: #5b9bd5;" bgcolor="#5b9bd5" >
+                <p  lang="es-ES" align="center"><span style="color: #ffffff;"><span ><span style="font-size: medium;"><strong>TIPO DE ORGANIZACI&Oacute;N</strong></span></span></span></p>
+                </td>
+                <td style="background: #5b9bd5;" bgcolor="#5b9bd5" >
+                <p  lang="es-ES" align="center"><span style="color: #ffffff;"><span ><span style="font-size: medium;"><strong>COOPERATIVAS</strong></span></span></span></p>
+                </td>
+                </tr>
+                <tr valign="top">
+                <td style="background: #deeaf6;" bgcolor="#deeaf6" >
+                <p  lang="es-ES"><span ><span style="font-size: medium;"><strong>DEFINICI&Oacute;N</strong></span></span></p>
+                </td>
+                <td style="background: #deeaf6;" bgcolor="#deeaf6">
+                <p  lang="es-ES"><span ><span style="font-size: medium;">Empresas asociativas sin &aacute;nimo de lucro, en las cuales los trabajadores o los usuarios, seg&uacute;n el caso, son simult&aacute;neamente los aportantes y los gestores de la empresa, creada con el objeto de producir o distribuir conjunta y eficientemente bienes o servicios para satisfacer las necesidades de sus asociados y de la comunidad en general.</span></span></p>
+                </td>
+                </tr>
+                <tr valign="top">
+                <td>
+                <p  lang="es-ES"><span ><span style="font-size: medium;"><strong>CLASES</strong></span></span></p>
+                </td>
+                <td>
+                <p ><span ><span style="font-size: medium;"><span lang="es-ES"><em><strong>Seg&uacute;n su objeto</strong></em></span></span></span><span ><span style="font-size: medium;"><span lang="es-ES">: de distribuci&oacute;n, de comercializaci&oacute;n, de trabajo asociado, agropecuarias, de educaci&oacute;n, de transporte, de vivienda.</span></span></span></p>
+                <p ><span ><span style="font-size: medium;"><span lang="es-ES"><em><strong>Seg&uacute;n los servicios</strong></em></span></span></span><span ><span style="font-size: medium;"><span lang="es-ES">: especializadas, multiactivas, integrales, de ahorro y cr&eacute;dito. </span></span></span></p>
+                </td>
+                </tr>
+                <tr valign="top">
+                <td style="background: #deeaf6;" bgcolor="#deeaf6">
+                <p  lang="es-ES"><span ><span style="font-size: medium;"><strong>ASOCIADOS</strong></span></span></p>
+                </td>
+                <td style="background: #deeaf6;" bgcolor="#deeaf6">
+                <p  lang="es-ES"><span ><span style="font-size: medium;">Personas naturales menores de edad que hayan cumplido 14 a&ntilde;os, o que sin haberlos cumplido se asocien por medio de representante legal, personas jur&iacute;dicas de desarrollo p&uacute;blico, personas jur&iacute;dicas del sector cooperativo, personas jur&iacute;dicas de derecho privado sin &aacute;nimo de lucro.</span></span></p>
+                </td>
+                </tr>
+                <tr valign="top">
+                <td>
+                <p ><span ><span style="font-size: medium;"><span lang="es-ES"><strong>No. M&Iacute;NIMO DE ASOCIADOS</strong></span></span></span></p>
+                </td>
+                <td>
+                <p  lang="es-ES"><span ><span style="font-size: medium;">20.</span></span></p>
+                </td>
+                </tr>
+                <tr valign="top">
+                <td style="background: #deeaf6;" bgcolor="#deeaf6" width="141" height="4">
+                <p  lang="es-ES"><span ><span style="font-size: medium;"><strong>DIRECCI&Oacute;N</strong></span></span></p>
+                </td>
+                <td style="background: #deeaf6;" bgcolor="#deeaf6" width="433">
+                <p  lang="es-ES"><span ><span style="font-size: medium;">Asamblea y concejo de administraci&oacute;n.</span></span></p>
+                </td>
+                </tr>
+                <tr valign="top">
+                <td width="141" height="83">
+                <p  lang="es-ES"><span ><span style="font-size: medium;"><strong>CONTROL</strong></span></span></p>
+                </td>
+                <td width="433">
+                <p  lang="es-ES"><span ><span style="font-size: medium;">Junta de vigilancia, revisor fiscal.</span></span></p>
+                <p ><span ><span style="font-size: medium;"><span lang="es-ES">Los &oacute;rganos de control podr&aacute;n exonerar revisor fiscal cuando las circunstancias economizas o de ubicaci&oacute;n geogr&aacute;fica o el n&uacute;mero de asociados lo justifiquen. </span></span></span></p>
+                </td>
+                </tr>
+                <tr valign="top">
+                <td style="background: #deeaf6;" bgcolor="#deeaf6" width="141" height="35">
+                <p  lang="es-ES"><span ><span style="font-size: medium;"><strong>REPRESENTANTE LEGAL</strong></span></span></p>
+                </td>
+                <td style="background: #deeaf6;" bgcolor="#deeaf6" width="433">
+                <p ><span ><span style="font-size: medium;"><span lang="es-ES">Gerente</span></span></span></p>
+                </td>
+                </tr>
+                <tr valign="top">
+                <td width="141" height="21">
+                <p  lang="es-ES"><span ><span style="font-size: medium;"><strong>ACTIVIDADES</strong></span></span></p>
+                </td>
+                <td width="433">
+                <p  lang="es-ES"><span ><span style="font-size: medium;">Todas las actividades seg&uacute;n la clase de cooperativa</span></span></p>
+                </td>
+                </tr>
+                <tr valign="top">
+                <td style="background: #deeaf6;" bgcolor="#deeaf6" width="141" height="13">
+                <p  lang="es-ES"><span ><span style="font-size: medium;"><strong>CAPACITACI&Oacute;N</strong></span></span></p>
+                </td>
+                <td style="background: #deeaf6;" bgcolor="#deeaf6" width="433">
+                <p  lang="es-ES"><span ><span style="font-size: medium;">20 horas</span></span></p>
+                </td>
+                </tr>
+                <tr valign="top">
+                <td width="141" height="28">
+                <p ><span ><span style="font-size: medium;"><span lang="es-ES"><strong>MARCO JUR&Iacute;DICO</strong></span></span></span></p>
+                </td>
+                <td width="433">
+                <p  lang="es-ES"><span ><span style="font-size: medium;">Ley 79 de 1988, ley 454 de 1998 y derechos reglamentarios </span></span></p>
+                </td>
+                </tr>
+                <tr valign="top">
+                <td style="background: #deeaf6;" bgcolor="#deeaf6" width="141" height="21">
+                <p  lang="es-ES"><span ><span style="font-size: medium;"><strong>VIGILANCIA</strong></span></span></p>
+                </td>
+                <td style="background: #deeaf6;" bgcolor="#deeaf6" width="433">
+                <p ><span ><span style="font-size: medium;"><span lang="es-ES">Supersolidaria, Ministerios y Superintendencias seg&uacute;n la actividad que desarrollan en su objeto social. </span></span></span></p>
+                </td>
+                </tr>
+                </tbody>
+            </table>
+            <!--3.1.-->
+            <h2 class="subtUnidad">3.2. CLASES DE COOPERATIVAS</h2>
+            <h3>CLASES DE COOPERATIVAS SEGÚN EL ÁMBITO DE LA ACTIVIDAD ECONÓMICA</h3>
+            <p><strong><span lang="es-ES">Especializadas</span></strong><span lang="es-ES"><strong>:</strong> son las que se organizan para atender una necesidad espec&iacute;fica, correspondiente a una sola rama de actividad econ&oacute;mica, social o cultural.</span> <span lang="es-ES">Estas organizaciones podr&aacute;n ofrecer servicios diferentes a los establecimientos en su objeto social, mediante la suscripci&oacute;n de convenios con otras entidades sin &aacute;nimo de lucro.</span></p>
+            <p align="justify"><strong><span lang="es-ES">Multiactivas:</span></strong><span lang="es-ES"> son las que se organizan para atender varias necesidades, mediante concurrencia de servicios en una sola entidad jur&iacute;dica. Los servicios deber&aacute;n ser organizados en secciones independientes, de acuerdo con las caracter&iacute;sticas de cada tipo especializado de actividad.</span></p>
+            <p align="justify"><strong><span lang="es-ES">Integrales</span></strong><span lang="es-ES"><strong>:</strong> son aquellas que, en desarrollo de su objeto social, realizan dos o m&aacute;s actividades conexas y complementarias entre s&iacute;, de producci&oacute;n, distribuci&oacute;n, consumo y prestaci&oacute;n de servicios.</span></p>
+            <h3>CLASES DE COOPERATIVAS SEGÚN LA ACTIVIDAD ECONÓMICA</h3>
+            <p><strong><span lang="es-ES">Producci&oacute;n:</span></strong><span lang="es-ES"> E</span><span lang="es-ES">stas organizaciones de producci&oacute;n son de gran importancia para el desarrollo socioecon&oacute;mico del pa&iacute;s, pues transforman los insumos en producto (por ejemplo: un cultivo de papa, la limpian, la embolsan y la venden) este proceso es de gran creatividad y trabajo lo cual permite obtener un mayor valor agregado para el crecimiento de la sociedad.</span></p>
+            <p><strong><span lang="es-ES">Consumo:</span></strong><span lang="es-ES"> En las organizaciones de consumo, la vinculaci&oacute;n debela ser abierta a todas las personas que puedan hacer uso de sus servicios y que acepten las responsabilidades inherentes a la asociaci&oacute;n. (por ejemplo: compra de maracuy&aacute;s en las veredas y luego la venta de estos en el pueblo).</span></p>
+            <!-------------------------------------- -->
+            
+            </div>
+      </div>
+      <div class="col-md-3 sidebar-outer" role="complementary">
+        <a href="#" data-toggle="collapse" data-target="#toggleDemo0" data-parent="#sidenav01" class="collapsed">
+                    <h4>
+                        Unidad 1
+                        <br>
+                        <small>HISTORIA DEL COOPERATIVISMO </small>
+                    </h4>
+                  </a>
+           
+
+            <ul id="tree3" >
+
+              <li>
+                  <a href="#" >
+                  <span class="glyphicon glyphicon-cloud"></span> 1.1.	Primera parte:  La cooperación y la solidaridad como un hecho recurrente en la historia
+                  </a>
+
+                    <ul class="">
+                        <li><a href="#">1.1.1.	La cooperación en la comunidad primitiva</a></li>
+                        <li><a href="#">1.1.2.	Formas de cooperación en la sociedad esclavista</a></li>
+                        <li><a href="#">1.1.3.	Otras asociaciones antiguas</a></li>
+                        <li><a href="#">1.1.4.	Formas de cooperación de la sociedad feudal</a></li>
+                    </ul>
+
+              </li>
+              <li >
+                  <a href="#" >
+                  <span class="glyphicon glyphicon-inbox"></span> 1.2.	Segunda parte: Precursores de las ideas cooperativas y la economía solidaria 
+                  </a>
+                    <ul class="">
+                        <li><a href="#">1.2.1.	Peter Cornelius Plocboy y las pequeñas Repúblicas</a></li>
+                        <li><a href="#">1.2.2.	Jhon Bellers y las colonias de trabajo colectivo</a></li>
+                        <li><a href="#">1.2.3.	Robert Owen: Las cooperativas integrales y los almacenes de consumo</a></li>
+                        <li><a href="#">1.2.4.	Charles Fourier: los Falansterios y Almacenes de Consumo</a></li>
+                    </ul>
+              </li>
+              <li><a href="#"><span class="glyphicon glyphicon-lock"></span> Actividades</a></li>
+              <li><a href="#"><span class="glyphicon glyphicon-calendar"></span> Evaluación</a></li>
+            </ul>
+            <a href="#top" class="back-to-top"> Regresar arriba </a> 
+      </div>
+   </div>
+</div>
+@endsection
+@section('scripts')
+    {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
+    <script >
+    $.fn.extend({
+    treed: function(o) {
+
+        var openedClass = 'glyphicon-minus-sign';
+        var closedClass = 'glyphicon-plus-sign';
+
+        if (typeof o != 'undefined') {
+            if (typeof o.openedClass != 'undefined') {
+                openedClass = o.openedClass;
+            }
+            if (typeof o.closedClass != 'undefined') {
+                closedClass = o.closedClass;
+            }
+        };
+
+        //initialize each of the top levels
+        var tree = $(this);
+        tree.addClass("tree");
+        tree.find('li').has("ul").each(function() {
+            var branch = $(this); //li with children ul
+            branch.prepend("<i class='indicator glyphicon " + closedClass + "'></i>");
+            branch.addClass('branch');
+            branch.on('click', function(e) {
+                if (this == e.target) {
+                    var icon = $(this).children('i:first');
+                    icon.toggleClass(openedClass + " " + closedClass);
+                    $(this).children().children().toggle();
+                }
+            })
+            branch.children().children().toggle();
+        });
+        //fire event from the dynamically added icon
+        tree.find('.branch .indicator').each(function() {
+            $(this).on('click', function() {
+                $(this).closest('li').click();
+            });
+        });
+        //fire event to open branch if the li contains an anchor instead of text
+        tree.find('.branch>a').each(function() {
+            $(this).on('click', function(e) {
+                $(this).closest('li').click();
+                e.preventDefault();
+            });
+        });
+        //fire event to open branch if the li contains a button instead of text
+        tree.find('.branch>button').each(function() {
+            $(this).on('click', function(e) {
+                $(this).closest('li').click();
+                e.preventDefault();
+            });
+        });
+    }
+});
+
+//Initialization of treeviews
+
+$('#tree1').treed();
+
+$('#tree2').treed({ openedClass: 'glyphicon-folder-open', closedClass: 'glyphicon-folder-close' });
+
+$('#tree3').treed({ openedClass: 'glyphicon-chevron-right', closedClass: 'glyphicon-chevron-down' });
+    </script>
+@endsection
